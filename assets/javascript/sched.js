35012a16bf6abc59b319eb81de8d073136f17b58
Anthony Cera <ace_cera@yahoo.com>
2018-01-11 22:54:30 -0800

    added table to html and form group to submit new train times.

---
+++ assets/javascript/sched.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyB6oa63DE5uMkIqqzJ3GYt2fCMV0bLFDcc",
+    authDomain: "trainhw-ed501.firebaseapp.com",
+    databaseURL: "https://trainhw-ed501.firebaseio.com",
+    projectId: "trainhw-ed501",
+    storageBucket: "trainhw-ed501.appspot.com",
+    messagingSenderId: "129559089522"
+  };
+  firebase.initializeApp(config);
+
+  var database = config;
