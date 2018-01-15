8ec1d7c2b45318e2ef95940ac1a002064e14d83b
Anthony Cera <ace_cera@yahoo.com>
2018-01-14 23:11:21 -0800

    forgot to commit a bunch of things. I added the on click function for the submit button, variables and call back functions.

--- assets/javascript/sched.js
+++ assets/javascript/sched.js
@@ -8,4 +8,44 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  var database = config;
+  var database = firebase.database();
+
+  $("#submit-button").on("click", function(event){
+      event.preventDefault();
+      var trainName = $(".trainNameInput").val().trim();
+      var trainDestination = $(".destinationInput").val().trim();
+      var trainTime = $(".firstTrainTimeInput").val().trim();
+      var trainFrequency = $(".frequencyInput").val().trim();
+
+      var newTrain = {
+          name: trainName,
+          destination: trainDestination,
+          time: trainTime,
+          frequency: trainFrequency,  
+      };
+
+      database.ref().push(newTrain);
+      console.log(newTrain.name);
+      console.log(newTrain.destination);
+      console.log(newTrain.time);
+      console.log(newTrain.frequency);
+
+      $(".trainNameInput").val("");
+      $(".destinationInput").val("");
+      $(".firstTrainTimeInput").val("");
+      $(".frequencyInput").val().trim();
+  });
+
+  database.ref().on("child_added"), function(childSnapshot, prevChildKey) {
+      console.log(childSnapshot.val());
+
+      var trainName = childSnapshot.val().name;
+      var trainDestination = childSnapshot.val().destination;
+      var trainTime = childSnapshot.val().time;
+      var trainFrequency = childSnapshot.val().frequency;
+
+      console.log(trainName);
+      console.log(trainDestination);
+      console.log(trainTime);
+      console.log(trainFrequency);
+  }
